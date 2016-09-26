23c69d1e1254377623c0e0abbddbbfdf76c7d533
Your name <Your email>
2016-09-26 02:04:00 -0400

    .

--- src/main/java/skeleton/RunMeFirst.java
+++ src/main/java/skeleton/RunMeFirst.java
@@ -8,28 +8,61 @@ import org.apache.commons.lang.text.StrSubstitutor;
 import org.apache.maven.cli.MavenCli;
 
 public class RunMeFirst {
+	private static final int FILE_LIMIT = 99;
+	private static String PROJECT_HOME = null;
 
-	private boolean compile() {
+	public static String getPROJECT_HOME() {
+		return PROJECT_HOME;
+	}
+
+	public static void setPROJECT_HOME(String pROJECT_HOME) {
+		PROJECT_HOME = pROJECT_HOME;
+	}
+
+	public static boolean compile() {
 		//=== c.f. https://commons.apache.org/proper/commons-lang/javadocs/api-2.6/org/apache/commons/lang/text/StrSubstitutor.html
+		String templateString = "The ${datatable1} jumped over the ${target}.\n${datatable1}";
 		Map valuesMap = new HashMap<Integer, String>();
-		valuesMap.put("animal", "quick brown fox");
-		valuesMap.put("target", "lazy dog");
-		String templateString = "The ${animal} jumped over the ${target}.";
+		String tableName = "datatable";
+		String currName = null;
 		StrSubstitutor sub = new StrSubstitutor(valuesMap);
-		String resolvedString = sub.replace(templateString);
-
+		for(int i = 0; i< FILE_LIMIT; i++) {
+			currName = tableName + 1;
+			if(isFileValid(getPROJECT_HOME() + "/" + currName + ".txt")) {
+				valuesMap.put("${" + currName + "}", "quick brown fox");
+				templateString = sub.replace(templateString);
+			}
+		}
+		String resolvedString = templateString;
+		
+		System.out.println("Final text = [" + resolvedString + "]");
+		
 		return false;
 	}
 
-	public static void main(String[] args) {
+	public static boolean isFileValid(String directoryOrFile) {
+		boolean ret = false;
+		File f = new File(directoryOrFile);
+		if(f.exists() || f.isDirectory()) {
+			ret = true;
+		}
+
+		return ret;
+	}
+
+	public static void main(String[] args) throws Exception {
 		String projDir = args[0];
-		File f = new File(projDir);
-		if(!f.exists() || !f.isDirectory()) {
+		if(projDir == null) {
+			throw new Exception("Need to specify the project directory!");
+		}
+		if(!isFileValid(projDir)) {
 			System.out.println("The specified project directory is invalid.");
 		}
+		setPROJECT_HOME(projDir);
 		MavenCli cli = new MavenCli();
 //		cli.doMain(new String[]{"clean", "install"}, "project_dir", System.out, System.out);
 		cli.doMain(new String[]{"test"}, args[0], System.out, System.out);
 
+		compile();
 	}
 }
