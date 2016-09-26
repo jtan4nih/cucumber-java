c04b8714a99ade12c5799d7f7b3d91973d6da29f
Your name <Your email>
2016-09-26 01:32:44 -0400

    .

--- src/main/java/skeleton/RunMeFirst.java
+++ src/main/java/skeleton/RunMeFirst.java
@@ -1,10 +1,25 @@
 package skeleton;
 
 import java.io.File;
+import java.util.Map;
+import java.util.HashMap;
 
+import org.apache.commons.lang.text.StrSubstitutor;
 import org.apache.maven.cli.MavenCli;
 
 public class RunMeFirst {
+
+	private boolean compile() {
+		//=== c.f. https://commons.apache.org/proper/commons-lang/javadocs/api-2.6/org/apache/commons/lang/text/StrSubstitutor.html
+		Map valuesMap = new HashMap<Integer, String>();
+		valuesMap.put("animal", "quick brown fox");
+		valuesMap.put("target", "lazy dog");
+		String templateString = "The ${animal} jumped over the ${target}.";
+		StrSubstitutor sub = new StrSubstitutor(valuesMap);
+		String resolvedString = sub.replace(templateString);
+
+		return false;
+	}
 
 	public static void main(String[] args) {
 		String projDir = args[0];
